72d5867c9e9eaf3f54b0c71bf4c96e88fdd68454
WolfY <david.skliarov.dev@gmail.com>
2025-07-13 03:52:20 +0400

    feat: implement task creation

---
+++ app/assets/js/script.js
@@ -0,0 +1,59 @@
+class Task {
+  constructor(text, done = false) {
+    this.text = text;
+    this.done = done;
+
+    this.element = this.createElement();
+  }
+
+  createElement() {
+    const tasks = document.getElementById("tasks");
+
+    const li = document.createElement("li");
+    li.className = "task task-active";
+
+    const taskBody = document.createElement("div");
+    taskBody.className = "task-body";
+
+    const tick = document.createElement("div");
+    tick.className = "tick";
+    tick.setAttribute("role", "checkbox");
+    tick.setAttribute("aria-checked", "false");
+    tick.tabIndex = 0;
+
+    const taskText = document.createElement("span");
+    taskText.className = "task-text";
+    taskText.textContent = this.text;
+
+    const taskActions = document.createElement("div");
+    taskActions.className = "task-actions";
+
+    const editBtn = document.createElement("button");
+    editBtn.className = "edit-btn";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete-btn";
+
+    li.append(taskBody, taskActions);
+    taskBody.append(tick, taskText);
+    taskActions.append(editBtn, deleteBtn);
+
+    deleteBtn.addEventListener("click", () => this.delete());
+
+    return li;
+  }
+}
+
+const tutorial = new Task("Press + button to add a task");
+const tasks = document.getElementById("tasks");
+tasks.append(tutorial.element);
+
+const addTaskBtn = document.getElementById("add-task-btn");
+
+addTaskBtn.addEventListener("click", function () {
+  const input = prompt("Write task description:");
+  if (input) {
+    const task = new Task(input);
+    tasks.append(task.element);
+  }
+});
