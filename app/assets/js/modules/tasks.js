ffcd0e556644483b908f63cb465c71e234311008
xWolfYx <david.skliarov.dev@gmail.com>
2025-08-21 03:17:59 +0400

    feat: implement modules

---
+++ app/assets/js/modules/tasks.js
@@ -0,0 +1,91 @@
+
+
+class Task {
+  constructor(text, done = false) {
+    this.text = text;
+    this.done = done;
+
+    this.element = this.createElement();
+  }
+
+  createElement() {
+    const tasks = document.getElementById("tasks");
+
+    const li = document.createElement("li");
+    li.className = "task";
+
+    const taskBody = document.createElement("div");
+    taskBody.className = "task-body";
+
+    const tick = document.createElement("div");
+    tick.className = "tick";
+    tick.setAttribute("role", "checkbox");
+    tick.setAttribute("aria-checked", "false");
+    tick.tabIndex = 0;
+
+    const taskText = document.createElement("span");
+    taskText.className = "task-text";
+    taskText.textContent = this.text;
+
+    const taskActions = document.createElement("div");
+    taskActions.className = "task-actions";
+
+    const editBtn = document.createElement("button");
+    editBtn.className = "edit-btn";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete-btn";
+
+    li.append(taskBody, taskActions);
+    taskBody.append(tick, taskText);
+    taskActions.append(editBtn, deleteBtn);
+
+    li.addEventListener("click", (e) => {
+      const target = e.target;
+      if (target.classList.contains("tick")) return this.toggleDone();
+      if (target.classList.contains("edit-btn")) return this.edit();
+      if (target.classList.contains("delete-btn")) return this.delete();
+    });
+
+    return li;
+  }
+
+  toggleDone() {
+    this.done = !this.done;
+    this.element.classList.toggle("checked");
+  }
+
+  edit() {
+    const editedInput = prompt();
+    if (editedInput) {
+      const taskText = this.element.querySelector(".task-text");
+      taskText.textContent = editedInput;
+    }
+  }
+
+  delete() {
+    const confirmation = confirm("Delete this task?");
+    if (confirmation) this.element.remove();
+  }
+}
+
+const tutorial = new Task("Press + button to add a task");
+const tasks = document.getElementById("tasks");
+tasks.append(tutorial.element);
+
+const addTaskBtn = document.querySelector(".add-task-btn");
+
+addTaskBtn.addEventListener("click", function () {
+  const input = prompt("Write task description:");
+  if (input) {
+    const task = new Task(input);
+    tasks.append(task.element);
+  }
+});
+
+addTaskBtn.addEventListener("click", () => {
+  const plusIcon = document.querySelector(".plus");
+  plusIcon.classList.remove("animate");
+  void plusIcon.offsetWidth;
+  plusIcon.classList.add("animate");
+});
