f9fa36a8ec063cebb9fe4bc725d7abc8531b8860
xWolfYx <david.skliarov.dev@gmail.com>
2025-09-04 03:10:22 +0400

    feat: add eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,6 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
 ]);
